536a2f753e2513ad28879883b49d91e11a077270
Rose Eiche <rose.eiche@voco.ee>
2024-12-02 14:46:13 +0200

    Ülesanne 2 - Lokaalsete kasutajate kustutamine süsteemist

---
+++ UL2_lokaalse_kasutaja_kustutamine.ps1
@@ -0,0 +1,32 @@
+﻿# Kasutaja käest palutakse sisestada ees-perenimi
+$eesnimi = Read-Host "Sisesta oma eesnimi kasutaja kustutamiseks"
+$perenimi = Read-Host "Sisesta oma perenimi kasutaja kustutamiseks"
+
+
+$kasutajanimi = ($eesnimi.ToLower() + "." + $perenimi.ToLower())
+
+# See kontrollib kas kasutaja on süsteemis olemas
+
+$userExists = Get-LocalUser -Name $kasutajanimi -ErrorAction SilentlyContinue
+
+If ($userExists) {
+    # Kui kasutaja on olemas, proovime kustutada antud kasutaja
+    try {
+        Remove-LocalUser -Name $kasutajanimi -ErrorAction Stop
+        Write-Host "Kasutaja $kasutajanimi on edukalt kustutatud."
+}
+    catch {
+  
+        Write-Host "Kasutaja $kasutajanimi kustutamine ebaõnnestus. Veateade :$_"
+    }
+  } else {
+      Write-Host "Kasutajat $kasutajanimi ei leitud süsteemist. Kustutamine ebaõnnestus."
+}
+
+
+# Kontrollime käsu õnnestumist süsteemi muutujaga $?
+if ($?) {
+    Write-Host "Käsk õnnestus."
+} else {
+   Write-Host "Käsk ebaõnnestus."
+}
