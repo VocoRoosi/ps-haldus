ee27af657de6a436a73eeb785166e5ff56f766f3
Rose Eiche <rose.eiche@voco.ee>
2024-12-02 13:09:14 +0200

    Kasutaja loomine 2

---
+++ UL2_kasutaja_haldus.ps1
@@ -0,0 +1,9 @@
+﻿param(
+    $username,
+    $fullname,
+    $description
+)
+
+$userPassword = ConvertTo-SecureString 'qwerty' -AsPlainText -Force
+
+New-LocalUser $username -Password $userPassword -FullName "$fullname" -Description "$description"
