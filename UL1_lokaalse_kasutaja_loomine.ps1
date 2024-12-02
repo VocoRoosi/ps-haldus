8e42f1164d4ce9a0210dca5ebf2d37479ba7dc51
Rose Eiche <rose.eiche@voco.ee>
2024-12-02 14:16:49 +0200

    Ülesanne 1 - lokaalse kasutaja loomine tema ees- ja perenimi järgi

---
+++ UL1_lokaalse_kasutaja_loomine.ps1
@@ -0,0 +1,27 @@
+﻿# Kasutaja käest palutakse sisestada ees-perenimi
+$eesnimi = Read-Host "Sisesta oma eesnimi"
+$perenimi = Read-Host "Sisesta oma perenimi"
+
+# Loome kasutajanime
+$kasutajanimi = ($eesnimi.ToLower() + "." + $perenimi.ToLower())
+
+# Loome täisnime ja kirjelduse
+$fullname = "$eesnimi $perenimi"
+$description = "Lokaalne konto"
+$password = "Parool1!" | ConvertTo-SecureString -AsPlainText -Force
+
+# Proovime luua kasutaja
+try {
+    New-LocalUser -Name $eesnimi -Password $password -FullName "$fullname" -Description "$description" -ErrorAction SilentlyContinue
+    Write-Host "Kasutaja $kasutajanimi on edukalt loodud."
+}
+catch {
+   Write-Host "Kasutaja loomine ebaõnnestus. Veateade: $_"
+}
+
+# Kontrollime käsu õnnestumist süsteemi muutujaga $?
+if ($?) {
+    Write-Host "Käsk õnnestus."
+} else {
+   Write-Host "Käsk ebaõnnestus."
+}
