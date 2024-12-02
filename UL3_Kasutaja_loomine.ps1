279889d63484f6443edce882bccfdf29c7f270c5
Rose Eiche <rose.eiche@voco.ee>
2024-12-02 13:48:14 +0200

    Kasutaja loomine 3

---
+++ UL3_Kasutaja_loomine.ps1
@@ -0,0 +1,11 @@
+﻿$file = "C:\Users\Rose\ps_haldus\users.csv"
+$users = Import-Csv $file -Encoding Default -Delimiter ";"
+foreach ($user in $users) {
+  $username = $user.Username
+  $fullname = $user.FullName
+  $description = $user.Description
+  $password = $user.Password | ConvertTo-SecureString -AsPlainText -Force
+  
+  New-LocalUser -Name $username -Password $password -FullName "$username" -Description "$description" -ErrorAction SilentlyContinue
+
+}
